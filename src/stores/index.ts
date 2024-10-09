548b280c26e938904144f6c5f4c20302995de8c2
Amwatah Obuya <obuyaamwatah@gmail.com>
2024-10-09 21:15:35 +0300

    create new supported currencies selection

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -3,11 +3,15 @@ import { proxy } from "valtio";
 type GlobalStoreType = {
   walletChoice: "Binance" | "Wallet";
   orderType: "Buy" | "Sell";
+  selectedFiatCurrency: "KES" | "NGN";
+  selectedCryptoToken: "USDT" | "USDC";
   selectedToken: "KES" | "NGN" | "USDT" | "USDC";
 };
 
 export const globalStore = proxy<GlobalStoreType>({
   walletChoice: "Wallet",
   orderType: "Buy",
+  selectedFiatCurrency: "KES",
+  selectedCryptoToken: "USDC",
   selectedToken: "USDC",
 });
