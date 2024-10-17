17fa4a19c5713ba8f6f6665da71edef77e488956
Amwatah Obuya <obuyaamwatah@gmail.com>
2024-10-17 21:31:02 +0300

    configure naira  payment setup  fields

--- src/app/_components/orders/WalletBuyNaira.tsx
+++ src/app/_components/orders/WalletBuyNaira.tsx
@@ -32,9 +32,9 @@ interface WalletOderProps {
 }
 
 export default function WalletBuyNaira({ orderId }: WalletOderProps) {
-  const [mpesaPaymentNumber, setMpesaPaymentNumber] = useState<
-    string | undefined
-  >(undefined);
+  const [paymentEmail, setPaymentEmail] = useState<string | undefined>(
+    undefined,
+  );
   const [walletType, setWalletType] = useState<"WALLET" | "BASENAME">("WALLET");
   const [basename, setBasename] = useState<string | undefined>(undefined);
   const connectionStatus = useActiveWalletConnectionStatus();
@@ -47,105 +47,8 @@ export default function WalletBuyNaira({ orderId }: WalletOderProps) {
   const { resolveBaseNameToWalletAddress, loading, resolvedAddress } =
     useResolveBaseNameToWalletAddress();
 
-  const intiateMpesaPayment = api.walletBuy.intiateWalletBuyOrder.useMutation({
-    onSettled: (data, _, variables) => {
-      if (!!data) {
-        notifications.show({
-          title: "Payment request sent",
-          message: `Please complete the payment  sent to +${variables.mpesaPaymentNumber}`,
-          color: "green",
-        });
-        setTimeout(() => {
-          checkStkPaymentStatus.mutate({ orderId: orderId });
-        }, 4000);
-      } else {
-        notifications.show({
-          title: "Error",
-          message: `An error occured in sending payment request`,
-          color: "red",
-        });
-      }
-    },
-  });
-
-  const checkStkPaymentStatus =
-    api.walletBuy.checkWalletBuyOrderSTKpaymentStatus.useMutation({
-      onSettled: (data, _, variables) => {
-        if (data === "PAYMENT MADE" || data === "PAYMENT CANCELED") {
-          notifications.hide("payment-loading");
-          if (data === "PAYMENT MADE") {
-            notifications.show({
-              title: "PAYMENT RECIEVED",
-              message: "Intiating crypto transfer",
-              color: "green",
-              autoClose: 10 * 1000,
-            });
-            intiateOrderSettlement.mutate({ orderId });
-          }
-          if (data === "PAYMENT CANCELED") {
-            notifications.show({
-              title: "PAYMENT CANCELED !",
-              message: "You canceled the payment request . Closing order",
-              color: "red",
-              autoClose: 6 * 1000,
-            });
-          }
-        } else {
-          setTimeout(() => {
-            checkStkPaymentStatus.mutate({ orderId: variables.orderId });
-          }, 5000);
-        }
-      },
-    });
-  const intiateOrderSettlement =
-    api.walletBuy.intiateOrderSettlement.useMutation({
-      onSettled: (data, _, variables) => {
-        if (data) {
-          closeOrder.mutate({ orderId: variables.orderId });
-          notifications.hide("sending-tokens");
-          notifications.show({
-            title: "Success",
-            message: "Crypto tokens sent to your wallet address",
-            color: "green",
-            autoClose: 15 * 1000,
-          });
-          router.push(`/orders/status/${orderId}`);
-        } else {
-          notifications.show({
-            title: "System error",
-            message: "We encountered an error sending tokens  to your address",
-            color: "red",
-            autoClose: 10 * 1000,
-          });
-        }
-      },
-    });
-  const closeOrder = api.walletBuy.closeOrder.useMutation();
-
   function handleMakePayment() {
-    if (
-      (account?.address ?? resolvedAddress) &&
-      mpesaPaymentNumber &&
-      mpesaPaymentNumber.length === 10 &&
-      (!isNaN(Number(mpesaPaymentNumber)) ||
-        mpesaPaymentNumber.startsWith("01"))
-    ) {
-      intiateMpesaPayment.mutate({
-        walletAddress:
-          walletType === "WALLET" && account?.address
-            ? account?.address
-            : walletType === "BASENAME" && resolvedAddress
-              ? resolvedAddress
-              : "",
-        orderId: orderId,
-        mpesaPaymentNumber: "254" + mpesaPaymentNumber.slice(1),
-      });
-    } else {
-      notifications.show({
-        title: "Invalid Mpesa Number",
-        message: "Enter a valid Mpesa number starting with 07..",
-      });
-    }
+    console.log("");
   }
   const handleResolve = async () => {
     await resolveBaseNameToWalletAddress({
@@ -153,28 +56,6 @@ export default function WalletBuyNaira({ orderId }: WalletOderProps) {
       basename: basename ?? "",
     });
   };
-
-  useEffect(() => {
-    if (checkStkPaymentStatus.isLoading) {
-      notifications.hide("payment-loading");
-      notifications.show({
-        id: "payment-loading",
-        title: "Checking Payment Status",
-        message: "Hold on while we process your request",
-        autoClose: 60 * 1000,
-        loading: true,
-      });
-    }
-    if (intiateOrderSettlement.isLoading) {
-      notifications.show({
-        id: "sending-tokens",
-        title: "Sending tokens",
-        message: "This may take a few seconds depending of  blockchain traffic",
-        autoClose: 60 * 1000,
-        loading: true,
-      });
-    }
-  }, [checkStkPaymentStatus.isLoading, intiateOrderSettlement.isLoading]);
 
   return (
     <Paper
@@ -195,15 +76,15 @@ export default function WalletBuyNaira({ orderId }: WalletOderProps) {
               />
             </h1>
             <Paper className=" flex h-fit w-32 items-center justify-around rounded-full p-1">
-              <h3>KES</h3>
+              <h3>NGN</h3>
               <Image
-                src={"/images/tokens/KES.png"}
-                alt="KES"
+                src={"/images/tokens/ngn.png"}
+                alt="NGN"
                 className=" h-8 w-8 rounded-full object-contain"
               />
             </Paper>
           </div>
-          <p className=" text-xs leading-[0]">FROM MPESA</p>
+          <p className=" text-xs leading-[0]">FROM PAYSTACK</p>
         </Card>
         <IoSwapVertical className="   place-self-center  justify-self-center " />
 
@@ -231,7 +112,7 @@ export default function WalletBuyNaira({ orderId }: WalletOderProps) {
                         ? "/images/tokens/FDUSD.svg"
                         : ""
                 }
-                alt="KES"
+                alt="USDT"
                 className=" h-8 w-8 rounded-full object-contain"
               />
             </Paper>
@@ -242,10 +123,10 @@ export default function WalletBuyNaira({ orderId }: WalletOderProps) {
 
       <TextInput
         size="lg"
-        label={<p className=" text-xs">Mpesa Number</p>}
-        placeholder="0712345678"
-        value={mpesaPaymentNumber}
-        onChange={(e) => setMpesaPaymentNumber(e.target.value)}
+        label={<p className=" text-xs">Your Email</p>}
+        placeholder="johndoe@gmail.com"
+        value={paymentEmail}
+        onChange={(e) => setPaymentEmail(e.target.value)}
       />
       <Accordion transitionDuration={1000} radius="xl">
         <Accordion.Item value="details">
@@ -303,7 +184,7 @@ export default function WalletBuyNaira({ orderId }: WalletOderProps) {
                     <FcInfo />
                   </span>
                   <span className="">
-                    {` 1 ${getOrderDetails.data?.swapToken}  =  ${getOrderDetails.data?.qoutedExchangeRate} KES`}
+                    {` 1 ${getOrderDetails.data?.swapToken}  =  ${getOrderDetails.data?.qoutedExchangeRate} NGN`}
                   </span>
                 </div>
               </Popover.Target>
@@ -325,28 +206,19 @@ export default function WalletBuyNaira({ orderId }: WalletOderProps) {
 
       {(account?.address && walletType == "WALLET") ??
       (walletType == "BASENAME" && resolvedAddress) ? (
-        getOrderDetails.data?.stkMessage?.stkStatus ? (
+        getOrderDetails.data?.payStackPaymentRefrence?.status === "SUCCESS" ? (
           <div className=" flex  items-center justify-center">
             <p>A payment request was intiatiated for this order</p>
           </div>
         ) : (
           <Button
-            disabled={
-              intiateMpesaPayment.isLoading ||
-              intiateMpesaPayment.isSuccess ||
-              !!getOrderDetails.data?.stkMessage?.stkStatus
-            }
-            loading={intiateMpesaPayment.isLoading}
+            disabled={false}
+            loading={false}
             onClick={() => handleMakePayment()}
             size="lg"
             className=" rounded-full"
           >
-            {checkStkPaymentStatus.data == "PAYMENT MADE" && "Payment Recieved"}
-            {checkStkPaymentStatus.data == "PAYMENT CANCELED" &&
-              "Payment Canceled"}
-            {!checkStkPaymentStatus.isLoading &&
-              !checkStkPaymentStatus.data &&
-              "Make Payment"}
+            Make Payment
           </Button>
         )
       ) : walletType === "WALLET" ? (
